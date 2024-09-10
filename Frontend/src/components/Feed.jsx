ff14f83bfe2fb06afcf6bd6d15c66b372dbeb79e
ayush-0012 <ayushbisht0012@gmail.com>
2024-09-10 13:18:03 +0530

    added userId in the profile div to redirect to the identify the user

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { LuPenSquare } from "react-icons/lu";
-import { CgProfile } from "react-icons/cg";
+import { FaUserCircle } from "react-icons/fa";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import userProfile from "../assets/profile4.jpg";
+import { CiUser } from "react-icons/ci";
 import axios from "axios";
 
 function Feed() {
@@ -46,32 +48,52 @@ function Feed() {
   return (
     <>
       {/* FEED NAVBAR */}
-      <div className="flex justify-between w-full h-[50px]  items-center lg:px-[100px] px-11 border-b border-bottom-2">
+      <div className="flex justify-between w-full h-[50px]  items-center px-4  border-b border-bottom-2">
         <h1 className="lg:text-4xl md:text-3xl text-2xl font-bold">Stories</h1>
         <div className="flex lg:justify-evenly justify-evenly w-[200px] h-[30px] items-center cursor-pointer">
           <div className="flex" onClick={() => handleWriteRedirect()}>
-            <LuPenSquare className="w-[40px] h-[25px]" />
+            <LuPenSquare className="w-7 h-6" />
             <p className="font-sans">Write</p>
           </div>
           <div>
-            <IoMdNotificationsOutline className="w-[40px] h-[25px]" />
+            <IoMdNotificationsOutline className="w-7 h-7" />
           </div>
-          <div className="relative ">
-            <button onClick={toggleDropdown} className="w-[40px] h-[25px]">
-              <CgProfile className="w-[40px] h-[25px]" />
+          <div className="relative">
+            <button onClick={toggleDropdown} className="w-6 h-6">
+              <FaUserCircle className="w-7 h-8 " />
             </button>
             {isOpen && (
-              <div className="absolute right-0  mt-2 w-72 bg-white border border-gray-300 shadow-lg rounded">
-                <ul>
-                  <li>
-                    <button
-                      onClick={handleLogout}
-                      className="w-full block px-4 py-2 text-gray-700 hover:bg-gray-100 "
-                    >
-                      Logout
-                    </button>
-                  </li>
-                </ul>
+              <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="options-menu"
+                >
+                  <a
+                    href="/profile"
+                    className="no-underline block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Profile
+                  </a>
+                  <a
+                    href="/settings"
+                    className="no-underline block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Account Settings
+                  </a>
+
+                  <div className="border-t border-gray-100"></div>
+                  <a
+                    onClick={() => handleLogout()}
+                    className="no-underline block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Logout
+                  </a>
+                </div>
               </div>
             )}
           </div>
@@ -80,7 +102,7 @@ function Feed() {
 
       {/* MAIN FEED DIV */}
 
-      <div className="flex">
+      <div className="flex mt-4">
         <div className="grid justify-center w-full">
           <div className="lg:w-[600px] ">
             {posts
@@ -98,36 +120,51 @@ function Feed() {
                 return (
                   // MAIN CONTENT DIV
                   <>
-                    <div className="flex">
+                    <Link to={`/post/${post._id}`} className="no-underline">
                       <div
-                        key={post._id}
-                        className="border-b lg:w-full md:w-[600px] sm:w-[600px] w-[400px] border-b-gray-300 mb-4"
+                        className="flex cursor-pointer"
+                        // onClick={() => navigate("/post")}
                       >
-                        <div className="flex">
-                          <img src="" alt="profilePic" className="mr-2" />
-                          <p>{post.userId.userName}</p>
-                        </div>
-                        {/* CONTENT DIV */}
-                        <div>
-                          <p className="text-2xl mb-3">{post.titleValue}</p>
-                          <p className="text-gray-500 font-sans line-clamp-2 overflow-hidden mb-6">
-                            {post.storyValue}
-                          </p>
-                        </div>
-                        {/* ACTION DIV */}
-                        <div className="flex justify-starts">
-                          <div className="mr-4 font-sans text-gray-600 text-[12px]">
-                            {formattedDate}
+                        <div
+                          key={post._id}
+                          className="border-b lg:w-full md:w-[600px] sm:w-[600px] w-[400px] px-10 border-b-gray-300 mb-4"
+                        >
+                          <Link
+                            to={`/profile/${post.userId._id}`}
+                            className="no-underline"
+                          >
+                            <div className="flex mb-3">
+                              <FaUserCircle className="mr-2 mt-2 w-8 h-8" />
+                              <p className="text-base font-sans font-bold mt-2">
+                                {post.userId.userName}
+                              </p>
+                            </div>
+                          </Link>
+
+                          {/* CONTENT DIV */}
+                          <div className="ml-3">
+                            <p className="text-2xl font-bold mb-3">
+                              {post.titleValue}
+                            </p>
+                            <p className="text-gray-500 font-sans line-clamp-2 overflow-hidden mb-6">
+                              {post.storyValue}
+                            </p>
                           </div>
-                          <div className="mr-4 font-sans text-gray-600 text-[12px]">
-                            Likes
-                          </div>
-                          <div className="mr-4 font-sans text-gray-600 text-[12px]">
-                            Comments
+                          {/* ACTION DIV */}
+                          <div className="flex justify-starts mb-4 ml-3">
+                            <div className="mr-4 font-sans text-gray-600 text-[12px]">
+                              {formattedDate}
+                            </div>
+                            <div className="mr-4 font-sans text-gray-600 text-[12px]">
+                              Likes
+                            </div>
+                            <div className="mr-4 font-sans text-gray-600 text-[12px]">
+                              Comments
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    </Link>
                   </>
                 );
               })}
