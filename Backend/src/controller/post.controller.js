5b0047b813386b93fa940d11bf79295eb9fdda17
ayush-0012 <ayushbisht0012@gmail.com>
2024-09-29 13:47:15 +0530

    wrote a controller to fetch all the posts created by a particular user to display them on the user's profile

--- Backend/src/controller/post.controller.js
+++ Backend/src/controller/post.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Post from "../model/post.model.js";
 import User from "../model/user.model.js";
 
@@ -29,7 +30,7 @@ export const createPost = async (req, res) => {
   }
 };
 
-//controller for fetching already created posts from the db
+//controller for fetching already created posts from the db to display them on the feed
 export const fetchPosts = async (req, res) => {
   try {
     const posts = await Post.find({}).populate("userId", "userName");
@@ -40,19 +41,39 @@ export const fetchPosts = async (req, res) => {
   }
 };
 
-//function to fetch all the details of a particular post
+//function to fetch all the details of a particular post of a particular user
 export const fetchPostDetail = async (req, res) => {
   const { id } = req.params;
   console.log(id);
 
   try {
     const post = await Post.findById(id).populate("userId", "userName").lean();
+
     console.log("fetched posts", post);
-    if (!post) {
-      return res.status(404).json({ message: "unable to fetch post" });
-    }
 
     res.status(200).json(post);
+  } catch (error) {
+    console.error("error fetching post: ", error);
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+//controller to fetch all the posts created by a user
+export const fetchUserPosts = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ message: "Invalid userId" });
+  }
+  try {
+    const posts = await Post.find({ userId })
+      .populate("userId", "userName")
+      .lean();
+
+    if (!posts || posts.length === 0) {
+      return res.status(404).json({ message: "No posts found for this user" });
+    }
+    res.status(200).json(posts);
   } catch (error) {
     console.error("error fetching post: ", error);
     res.status(500).json({ message: "server error" });
