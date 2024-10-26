ea6b79e93091fdcb990d8a4dfe5da327eee0c625
ayush-0012 <ayushbisht0012@gmail.com>
2024-10-26 12:22:31 +0530

    UI changes

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -12,14 +12,14 @@ const Hero = () => {
       <div className="w-full h-full flex  ">
         {/* MID SECTION */}
         <div className="lg:w-full md:w-full md:h-[600px] lg:py-[170px] lg:px-[100px] md:py-[170px] md:px-[40px] sm:py-[160px] sm:px-[50px] py-[200px] px-[40px]">
-          <h1 className="lg:text-7xl md:text-7xl sm:text-7xl text-[38px] font-bold text-gray-800">
+          <h1 className="lg:text-7xl md:text-7xl sm:text-7xl text-[38px] font-bold text-white">
             Your Stories and Experiences
           </h1>
-          <p className=" lg:text-xl md:text-xl sm:text-lg text-lg py-4 lg:pl-6 md:pl-4 pl-2">
+          <p className="text-gray-300 lg:text-xl md:text-xl sm:text-lg text-lg py-4 lg:pl-6 md:pl-4 pl-2">
             A space to read, write and share about you and your life.
           </p>
           <button
-            className="rounded-full h-9 border-2 text-white bg-black border-black w-[120px] lg:ml-6 md:ml-4 ml-2"
+            className="rounded-full h-9 border-2 text-black bg-white border-black w-[120px] lg:ml-6 md:ml-4 ml-2"
             onClick={() => handleSignupRedirect()}
           >
             Get Started
