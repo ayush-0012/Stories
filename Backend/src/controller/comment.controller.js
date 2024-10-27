a593a16dc843a660a1a77bf4f4ed7a51238bb0f3
ayush-0012 <ayushbisht0012@gmail.com>
2024-10-27 20:34:08 +0530

    wrote a controller to like and unlike a comment

--- Backend/src/controller/comment.controller.js
+++ Backend/src/controller/comment.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Comment from "../model/comments.model.js";
 import Post from "../model/post.model.js";
 import User from "../model/user.model.js";
@@ -41,5 +42,55 @@ export const addCommentsToPost = async (req, res) => {
   } catch (error) {
     console.log("an error occured", error);
     res.status(500).json({ message: "server error" });
+  }
+};
+
+//to like a comment under a post
+export const likeComment = async (req, res) => {
+  const { commentId, userId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(400).json({ message: "user not found " });
+    }
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      res.status(400).json({ message: "comment not found" });
+    }
+
+    const userIdObj = new mongoose.Types.ObjectId(userId);
+
+    const hasLikedComment = comment.likesOnComment.some((id) =>
+      id.equals(userIdObj)
+    );
+    if (hasLikedComment) {
+      // Remove the user's like
+      comment.likesOnComment = comment.likesOnComment.filter(
+        (id) => !id.equals(userIdObj)
+      );
+      console.log("Unliked the comment");
+    } else {
+      comment.likesOnComment.push(userIdObj);
+      console.log("liked the comment");
+    }
+
+    await comment.save();
+
+    await comment.populate("likesOnComment", "userName");
+
+    res.status(200).json({
+      message: hasLikedComment ? "unliked the comment" : "liked the comment",
+      comment: comment,
+      likes: comment.likesOnComment.length,
+      hasliked: !hasLikedComment,
+      likedBy: comment.likesOnComment,
+    });
+  } catch (error) {
+    console.log("error liking the comment", error);
+    res
+      .status(500)
+      .json({ message: "error liking the comment", error: error.message });
   }
 };
