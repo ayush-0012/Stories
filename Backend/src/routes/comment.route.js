09bdc18e32f060ac4d0e7037443ef6330b7d33fa
ayush-0012 <ayushbisht0012@gmail.com>
2024-11-02 23:12:49 +0530

    created a get route to fetch likes and status of comments

--- Backend/src/routes/comment.route.js
+++ Backend/src/routes/comment.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { likeComment } from "../controller/comment.controller.js";
+import {
+  fetchCommentLikes,
+  likeComment,
+} from "../controller/comment.controller.js";
 
 const router = express.Router();
 
 router.patch("/api/comment/likes", likeComment);
+router.get("/api/comment/likes/:commentId", fetchCommentLikes);
 
 export const commentRoutes = router;
