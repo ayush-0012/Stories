42444292d23649973f12e09aa56598251da85c37
ayush-0012 <ayushbisht0012@gmail.com>
2024-11-18 21:42:35 +0530

    minor changes

--- Frontend/src/components/Feed.jsx
+++ Frontend/src/components/Feed.jsx
@@ -177,13 +177,13 @@ function Feed() {
           </div>
         </header>
 
-        <div className="container flex">
+        <div className="container mx-auto flex">
           {/* Sidebar */}
           <CommonNav />
 
           {/* Main Content */}
-          <main className="flex-1 md:ml-64">
-            <div className="max-w-6xl mx-auto p-4 space-y-4 mb-16">
+          <main className="flex-1 w-full md:ml-64 mb-16">
+            <div className="max-w-6xl mx-auto p-4 space-y-4">
               {posts
                 .slice()
                 .reverse()
