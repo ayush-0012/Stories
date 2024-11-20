b55e3dac8882601d9fee99da51c4c44836a11b4a
ayush-0012 <ayushbisht0012@gmail.com>
2024-11-20 20:59:58 +0530

    UI changes

--- Frontend/src/utils/formatDate.js
+++ Frontend/src/utils/formatDate.js
@@ -6,12 +6,12 @@ const formatDate = (dateString) => {
   const diffInSeconds = Math.floor((now - date) / 1000);
 
   if (diffInSeconds < 60) return "just now";
-  if (diffInSeconds < 3600) return `${Math.floor(diffInSeconds / 60)} m `;
-  if (diffInSeconds < 86400) return `${Math.floor(diffInSeconds / 3600)} h `;
-  if (diffInSeconds < 2592000) return `${Math.floor(diffInSeconds / 86400)} d `;
+  if (diffInSeconds < 3600) return `${Math.floor(diffInSeconds / 60)}m `;
+  if (diffInSeconds < 86400) return `${Math.floor(diffInSeconds / 3600)}h `;
+  if (diffInSeconds < 2592000) return `${Math.floor(diffInSeconds / 86400)}d `;
   if (diffInSeconds < 31536000)
-    return `${Math.floor(diffInSeconds / 2592000)} m `;
-  return `${Math.floor(diffInSeconds / 31536000)} y `;
+    return `${Math.floor(diffInSeconds / 2592000)}m `;
+  return `${Math.floor(diffInSeconds / 31536000)}y `;
 };
 
 export default formatDate;
