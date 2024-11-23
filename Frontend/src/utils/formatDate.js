0aed7b9d6689edca4c18b41e724a255869993090
ayush-0012 <ayushbisht0012@gmail.com>
2024-11-23 23:47:03 +0530

    minor change

--- Frontend/src/utils/formatDate.js
+++ Frontend/src/utils/formatDate.js
@@ -10,7 +10,7 @@ const formatDate = (dateString) => {
   if (diffInSeconds < 86400) return `${Math.floor(diffInSeconds / 3600)}h `;
   if (diffInSeconds < 2592000) return `${Math.floor(diffInSeconds / 86400)}d `;
   if (diffInSeconds < 31536000)
-    return `${Math.floor(diffInSeconds / 2592000)}m `;
+    return `${Math.floor(diffInSeconds / 2592000)}mo `;
   return `${Math.floor(diffInSeconds / 31536000)}y `;
 };
 
